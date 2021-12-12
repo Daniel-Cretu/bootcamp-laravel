9b8e950d001ed4dfe8dc86f85a0768b0afb7c582
Daniel-Cretu <danielcretu1998@gmail.com>
2021-12-12 19:31:29 +0200

    Added Category and Product models

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+    ];
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+
+    public $timestamps = false;
+}
