a8191825333d6fcb34caf4a05037b318efce3d97
Daniel-Cretu <danielcretu1998@gmail.com>
2021-12-12 18:22:49 +0200

    Created migration files

---
+++ database/migrations/2021_12_12_171823_create_warnings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWarningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warnings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',40);
+            $table->tinyText('image_location')->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warnings');
+    }
+}
---
+++ database/migrations/2021_12_12_171955_create_product_warning_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductWarningTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_warning', function (Blueprint $table) {
+            $table->foreignId('product_id')
+                ->references('id')->on('products')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->bigInteger('warning_id')->unsigned()->nullable(false);
+            $table->foreign('warning_id')
+                ->references('id')->on('warnings')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_warning');
+    }
+}
