a5439efef76b237c18b3a76d7151e274d9e8bcee
Daniel-Cretu <danielcretu1998@gmail.com>
2021-12-16 15:47:08 +0200

    Generating images and showing them on each article

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -16,14 +16,16 @@ class ArticleFactory extends Factory
      */
     public function definition()
     {
+        $image = $this->faker->image('storage/app/public');
+        $imageName = pathinfo($image, PATHINFO_FILENAME) . '.' . pathinfo($image, PATHINFO_EXTENSION);
         return [
             'title' => $this->faker->sentence(),
             'description' => $this->faker->paragraph(),
+            'blog_category_id' => BlogCategory::factory(),
             'author_id' => User::factory(),
-            'image' => $this->faker->image('storage/app/public'),
             'published_at' => $this->faker->dateTime(),
             'excerpt' => $this->faker->sentence(),
-            'blog_category_id' => BlogCategory::factory(),
+            'image' => $imageName,
             'seo_title' => $this->faker->sentence(),
             'seo_description' => $this->faker->sentence(),
         ];
