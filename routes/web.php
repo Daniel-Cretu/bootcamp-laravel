a09227084b7d6c4c5285ce9b107b5d19c44269ea
Daniel CRETU <danielcretu1998@gmail.com>
2021-12-20 11:02:41 +0000

    Added new web routes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +21,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::get('/article', [ArticleController::class, 'index'])->name('article');
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
+Route::get('/product', [ProductController::class, 'index'])->name('product');
