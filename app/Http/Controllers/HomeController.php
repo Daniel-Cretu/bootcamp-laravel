30a65f4c2f3282641a18c24e9720c3e6aa4b8d9a
Daniel CRETU <danielcretu1998@gmail.com>
2021-12-20 13:48:50 +0200

    Removed some comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,5 @@ class HomeController extends Controller
     {
         $articles = Article::orderby('created_at', 'DESC')->limit(3)->get()->all();
         return view('pages.home', ['articles' => $articles]);
-//        return view('pages.home');
     }
 }
