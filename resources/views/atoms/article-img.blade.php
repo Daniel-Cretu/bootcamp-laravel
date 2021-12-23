236a4167cb1b7b296cf42199a9e5fbfcae3afd2b
Daniel CRETU <danielcretu1998@gmail.com>
2021-12-23 14:27:02 +0200

    Cleaned some comments

--- resources/views/atoms/article-img.blade.php
+++ resources/views/atoms/article-img.blade.php
@@ -1,6 +1,1 @@
-{{--@if(isset($image))--}}
-{{--    <img src="{{asset('storage/'.$image)}}" alt="Product" class="img-fluid  border rounded-3">--}}
-{{--@else--}}
-{{--    <img src="{{asset('storage/0adca39a30173f4dca12b77280b32a8b.png')}}" alt="Product" class="img-fluid  border rounded-3">--}}
-{{--@endif--}}
 <img src="{{$article->image_url}}" alt="Product" class="img-fluid  border rounded-3 w-100">
--- resources/views/molecules/article.blade.php
+++ resources/views/molecules/article.blade.php
@@ -10,17 +10,10 @@
     <div class="row m-0 p-0">
         <h6 class="col-12 m-0 px-3 px-lg-4 py-0">{{date('d-M-y', strtotime($article->published_at))}}</h6>
         <h2 class="col-12 m-0 px-3 px-lg-4 py-0">{{$article->title}}</h2>
-{{--        <div class="row">--}}
-{{--            <p class="float-start m-4 ms-0 w-25">--}}
-{{--            <p class="float-md-start m-4 ms-0 w-md-25">--}}
-{{--                @include('../atoms.article-img', ['image' => $article->image])--}}
-{{--            </p>--}}
-{{--            <p class="lead m-4 ms-0 ">{{$article->excerpt}}</p>--}}
             <p class="col-12 col-md-6 col-lg-4 px-3 px-lg-4 py-2">
                 @include('../atoms.article-img', ['image' => $article->image])
             </p>
             <p class="col-12 col-md-6 col-lg-8 px-3 px-lg-4 py-2">{{$article->excerpt}}</p>
-{{--        </div>--}}
     </div>
     <a class="col-12 col-xl-6 btn btn-outline-secondary" type="button" href="{{route('article',['articleId' => $article->id])}}">Continue reading ...</a>
 </div>
