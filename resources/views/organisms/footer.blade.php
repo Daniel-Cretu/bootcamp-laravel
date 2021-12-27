ffcd0fb7d2999a1a6a136d28ad01001f5a215932
Daniel CRETU <danielcretu1998@gmail.com>
2021-12-27 13:53:10 +0200

    Fixed footer margins

--- resources/views/organisms/footer.blade.php
+++ resources/views/organisms/footer.blade.php
@@ -11,7 +11,7 @@
 {{--        </div>--}}
 {{--    </div>--}}
 {{--</footer>--}}
-<footer class="p-0 m-0 py-3 mb-4 container-xxl bg-light layered-box">
+<footer class="py-3 mb-4 container-xxl bg-light layered-box">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item"><a href="{{route('home')}}" class="nav-link px-2 text-muted">Home</a></li>
         <li class="nav-item"><a href="{{route('menu')}}" class="nav-link px-2 text-muted">Menu</a></li>
