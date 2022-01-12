6a462968683ecc9a17ceab7f53fe808d2d91ac59
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-12 13:40:06 +0200

    Renamed index method to view. Added send method that sends an email.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Mail\Message;
 
 class ContactController extends Controller
 {
-    public function index()
-    {
+    public function view(){
         return view('pages.contact');
+    }
+    public function send(ContactRequest $request): RedirectResponse
+    {
+        $data = $request->validated();
+        \Log::debug('test', $data);
+        \Mail::send(
+            'molecules.emailContact',
+            [
+                'email' => $data['email'],
+                'name' => $data['name'],
+                'messageText' => $data['message'],
+
+            ],
+            function (Message $message) use ($data) {
+                $message->subject('Message from ' . $data['email']);
+                $message->to('contact@pizzaslice.org');
+                $message->from('no-reply@pizzaslice.org', 'Pizza Slice mailer');
+            }
+        );
+
+        return redirect()->route('contact')->withInput($data);
     }
 }
