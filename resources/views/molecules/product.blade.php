d088f68efd59a2763554a1af95e15277810233bd
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-12 14:33:50 +0200

    Added cart button for product.

--- resources/views/molecules/product.blade.php
+++ resources/views/molecules/product.blade.php
@@ -1,8 +1,16 @@
-<div class="product bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    <div class="link-light">
+<div class="product bg-light h-100 d-flex flex-column justify-content-between my-xl-3">
+    <div class="product-info" data-bs-toggle="modal" data-bs-target="#exampleModal">
         @include('../atoms.product-img',['productImageLocation' => $product->image_location])
-        <h4 class="title text-shadow">{{$product->name}}</h4>
-        <h4 class="price text-shadow">{{$product->price}} lei</h4>
+        <h4 class="title px-2">{{$product->name}}</h4>
+    </div>
+    <div class="d-flex flex-row justify-content-between align-middle px-2">
+        <h4 class="price my-auto">{{$product->price}} lei</h4>
+        <button class="btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="cart bi bi-cart-plus" viewBox="0 0 16 16">
+                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+            </svg>
+        </button>
     </div>
 </div>
 
