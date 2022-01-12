b01a3f5d24c1c389485d453ff5a034f06e190fcd
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-12 13:57:00 +0200

    Removed old commented code.

--- resources/views/molecules/product.blade.php
+++ resources/views/molecules/product.blade.php
@@ -1,16 +1,10 @@
 <div class="product bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
-{{--    <a href="{{route('product',['productId' => $product->id])}}" class="link-light">--}}
     <div class="link-light">
         @include('../atoms.product-img',['productImageLocation' => $product->image_location])
         <h4 class="title text-shadow">{{$product->name}}</h4>
         <h4 class="price text-shadow">{{$product->price}} lei</h4>
     </div>
 </div>
-
-{{--<!-- Button trigger modal -->--}}
-{{--<button type="button" class="btn btn-primary">--}}
-{{--    Launch demo modal--}}
-{{--</button>--}}
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -20,29 +14,9 @@
                 <h5 class="modal-title" id="exampleModalLabel">{{$product->name}}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-{{--            <div class="modal-body">--}}
-{{--                ...--}}
-{{--            </div>--}}
-{{--            <div class="modal-footer">--}}
-{{--                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>--}}
-{{--                <button type="button" class="btn btn-primary">Save changes</button>--}}
-{{--            </div>--}}
-
-
-
-
-
-
-
-
-
-
-
-
             <div class="mb-4 bg-light rounded-3">
                 <div class="container-fluid">
                     <h1 class="m-4"></h1>
-                    {{--            <h6>{{$product[0]}}</h6>--}}
                     <div class="row">
                         <div class="col-12 col-lg-6">
                                                 @include('../atoms.product-img',['productImageLocation' => $product->image_location])
@@ -57,27 +31,8 @@
             </div>
             <div class="row p-0 m-0 d-flex flex-wrap ">
                 <h3 class="my-4">Toppings</h3>
-{{--                @for ($i = 0; $i < 4; $i++)--}}
-{{--                    <div class="row my-2 align-items-center justify-content-between">--}}
-{{--                        <div class="col-12 col-lg-8 my-2 my-lg-0 text-center text-lg-start">Topping {{$i+1}}</div>--}}
-{{--                        <div class="col-12 col-lg-4 d-flex align-items-center justify-content-center justify-content-lg-end">--}}
-{{--                            <div class="btn btn-warning text-light topping-add-rm">-</div>--}}
-{{--                            <div class="mx-5">2</div>--}}
-{{--                            <div class="btn btn-warning text-light topping-add-rm">+</div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                @endfor--}}
                 <div class="row row-cols-2 p-0 m-0">
                 @for ($i = 0; $i < 4; $i++)
-
-{{--                        <div class="col-12 col-lg-8 my-2 my-lg-0 text-center text-lg-start">Topping {{$i+1}}</div>--}}
-{{--                        <div class="col-12 col-lg-4 d-flex align-items-center justify-content-center justify-content-lg-end">--}}
-{{--                            <div class="btn btn-warning text-light topping-add-rm">-</div>--}}
-{{--                            <div class="mx-5">2</div>--}}
-{{--                            <div class="btn btn-warning text-light topping-add-rm">+</div>--}}
-{{--                            --}}
-{{--                        </div>--}}
-{{--                        <div class="col-8">Topping {{$i+1}}</div>--}}
                         <div class="col my-2 d-flex flex-row justify-content-center">
                             <div class="form-check">
                                 <input class="form-check-input mx-3" type="checkbox" id="topping{{$i+1}}">
@@ -95,15 +50,6 @@
                 </div>
             </div>
             <button class="btn m-3 btn-outline-secondary btn-lg" type="button">Add</button>
-
-
-
-
-
-
-
-
-
         </div>
     </div>
 </div>
