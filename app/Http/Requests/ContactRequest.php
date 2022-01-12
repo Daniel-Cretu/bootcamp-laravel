7bf0e8f59205bc6829aeb52b617f7f1842481f6e
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-12 13:36:13 +0200

    Updated Contact Request rules based on application contact form.

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -2,9 +2,8 @@
 
 namespace  App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
-class ContactUsRequest extends FormRequest
+class ContactRequest extends FormRequest
 {
     public function authorize():bool
     {
@@ -15,12 +14,6 @@ class ContactUsRequest extends FormRequest
         return [
             'email' => ['string', 'email', 'required'],
             'name' => ['min:2', 'required', 'string'],
-            'department' => [
-                'required',
-                'string',
-                Rule::in(['administration', 'accounting', 'tehnicalDepartment', 'logistic']),
-            ],
-            'districts' => ['required', 'array', 'min:1', 'in:chishinau,orhei,straseni'],
             'message' => ['required', 'string', 'min:10'],
             'readTerms' => ['required'],
         ];
