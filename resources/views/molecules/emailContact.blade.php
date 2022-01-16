6067637a8229cd56ebeaa4d65b88f811610df81d
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-16 13:38:00 +0200

    Added sender email to email template.

--- resources/views/molecules/emailContact.blade.php
+++ resources/views/molecules/emailContact.blade.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <body>
-    <p>Message: {{ $messageText }}</p>
     <p>Subject: {{ $subject }}</p>
+    <br>
+    <p>Message: {{ $messageText }}</p>
+    <hr>
     <p>From: {{ $name }}</p>
+    <p>Email: {{ $email }}</p>
     </body>
 </html>
