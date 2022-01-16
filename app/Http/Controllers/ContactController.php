3377997326188096f40e083c2d3913fb3fc4f67d
Daniel-Cretu <89217076+Daniel-Cretu@users.noreply.github.com>
2022-01-16 17:54:11 +0200

    Revert "Modify email example"

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,35 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ContactRequest;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Mail\Message;
+use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function view(){
-        return view('pages.contact');
-    }
-    public function send(ContactRequest $request): RedirectResponse
+    public function index()
     {
-        $data = $request->validated();
-        \Log::debug('test', $data);
-        \Mail::send(
-            'molecules.emailContact',
-            [
-                'email' => $data['email'],
-                'name' => $data['name'],
-                'messageText' => $data['message'],
-                'subject' => $data['subject']
-
-            ],
-            function (Message $message) use ($data) {
-                $message->subject('Message from ' . $data['email']);
-                $message->to('contact@pizzaslice.org');
-                $message->from('no-reply@pizzaslice.org', 'Pizza Slice mailer');
-            }
-        );
-
-        return redirect()->route('contact')->withInput($data);
+        return view('pages.contact');
     }
 }
