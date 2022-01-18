18b070dad0067296f6d12da2be2d1cfef1332754
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-18 22:35:22 +0200

    Adjusted ArticleController for ModelLogger

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\Comment;
 use App\Models\LoggableInterface;
+use App\Services\ModelLogger;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Psr\Log\LoggerInterface;
@@ -12,17 +13,12 @@ use Psr\Log\LoggerInterface;
 class ArticleController extends Controller
 {
 
-    public function show($articleId, Request $request, LoggerInterface $logger)
+    public function show($articleId, Request $request, ModelLogger $logger)
     {
         $article = Article::findOrFail($articleId);
         $articleComments = Comment::orderby('created_at', 'DESC')->paginate(1)->onEachSide(1);
 
-        $user = $request->user();
-        $userRepresentation = $user ? "User with id {$user->id}" : "Unknown User";
-        $logger->info(
-            $userRepresentation . ' accessed ' . "article with id {$articleId}",
-            ["id" => $articleId, 'title' => $article->title],
-        );
+        $logger->logModel($request->user(), $article);
 
         return view('pages.article', ['articleComments' => $articleComments, 'article' => $article]);
     }
