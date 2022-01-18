1e29bc130e248d6a34a18544568b01003ba61472
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-18 21:18:28 +0200

    Integrate in ArticleController logging of Article read.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Comment;
+use App\Models\LoggableInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Psr\Log\LoggerInterface;
 
 class ArticleController extends Controller
 {
 
-    public function show($articleId)
+    public function show($articleId, Request $request, LoggerInterface $logger)
     {
         $article = Article::findOrFail($articleId);
         $articleComments = Comment::orderby('created_at', 'DESC')->paginate(1)->onEachSide(1);
+
+        $user = $request->user();
+        $userRepresentation = $user ? "User with id {$user->id}" : "Unknown User";
+        $logger->info(
+            $userRepresentation . ' accessed ' . "article with id {$articleId}",
+            ["id" => $articleId, 'title' => $article->title],
+        );
+
         return view('pages.article', ['articleComments' => $articleComments, 'article' => $article]);
     }
 }
