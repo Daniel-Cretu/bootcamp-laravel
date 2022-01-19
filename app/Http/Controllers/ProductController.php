b82740b555a40d19a6343d8a33fac784bc032da1
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-19 11:54:32 +0200

    Added getProduct and addProduct methods. Implemented logger in addProduct method.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Services\ModelLogger;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -11,5 +12,17 @@ class ProductController extends Controller
     {
         $product = Product::with('ProductInfo')->where('id',$productId)->get()->all();
         return view('pages.product',['product' => $product]);
+    }
+
+    public function getProduct($productId)
+    {
+        return Product::with('ProductInfo')->findOrFail($productId);
+    }
+
+    public function addProduct($productId,Request $request, ModelLogger $logger)
+    {
+        $product = Product::with('ProductInfo')->findOrFail($productId);
+        $logger->logModel($request->user(), $product);
+        return back();
     }
 }
