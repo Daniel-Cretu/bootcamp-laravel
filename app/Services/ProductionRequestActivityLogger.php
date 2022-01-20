7b59150b888fad2f1cade186828d25e4ab22639d
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-20 11:59:01 +0200

    Added Debug and Production request activity loggers.

---
+++ app/Services/ProductionRequestActivityLogger.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ProductionRequestActivityLogger extends AbstractRequestActivityLogger
+{
+    protected function collectRequestData(Request $request): array
+    {
+        return ["Url" => $request->fullUrl() ,"Time" => Carbon::now()];
+    }
+}
