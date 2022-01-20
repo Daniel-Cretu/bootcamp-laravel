c227cc4938d980dd946ccd284ce3cb96f2f3c556
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-20 14:14:39 +0200

    Fixed wrong product redirect.

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -7,7 +7,8 @@ use App\Models\Product;
 class MenuController extends Controller
 {
     public function index() {
-        $products = Product::join('products_info', 'products_info.product_id', '=', 'products.id')
+        $products = Product::select('products.id', 'products.name', 'products.price', 'products.category_id', 'products_info.image_location')
+            ->join('products_info', 'products_info.product_id', '=', 'products.id')
             ->where('products.flag', '1')
             ->orderby('name', 'ASC')
             ->get();
