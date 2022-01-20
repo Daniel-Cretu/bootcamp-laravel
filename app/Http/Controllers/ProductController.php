655e8e9600eea932ea25e4fe0e761bab17a026bc
Daniel CRETU <danielcretu1998@gmail.com>
2022-01-20 14:47:21 +0200

    Fixed wrong select. Now redirects if accessed closed or wrong product.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,12 @@ class ProductController extends Controller
 {
     public function index($productId)
     {
-        $product = Product::with('ProductInfo')->where('id',$productId)->get()->all();
+        if(Product::select('flag')->find($productId) === null || !(Product::select('flag')->find($productId)->flag === 1))
+        {
+            return redirect('/');
+        }
+        $product = Product::select('products.id', 'products.category_id', 'products.name', 'products.price', 'products_info.description', 'products_info.image_location')
+            ->with('ProductInfo')->join('products_info', 'products_info.product_id', '=', 'products.id')->findOrFail($productId);
         return view('pages.product',['product' => $product]);
     }
 }
